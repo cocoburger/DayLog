ceb2f6502a05c5239e08978749c361c55c8ba8c2
inbum <1234!@>
2022-05-10 17:56:40 +0900

    7.4.5 완료

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,18 +1,27 @@
 import React, {useContext} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import SearchContext from '../contexts/SearchContext';
+import LogContext from '../contexts/LogContext';
+import FeedList from '../components/FeedList';
 
 function SearchScreen({navigation}) {
-    const {keyword} = useContext(SearchContext);
-    return (
-        <View style={styles} >
-            <Text>{keyword}</Text>
-        </View>
-    );
+  const {keyword} = useContext(SearchContext);
+  const {logs} = useContext(LogContext);
+  const filtered =
+    keyword === ''
+      ? []
+      : logs.filter(log =>
+          [log.title, log.body].some(text => text.includes(keyword)),
+        );
+  return (
+    <View style={styles.block}>
+      <FeedList logs={filtered} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    block: {},
-})
+  block: {flex: 1},
+});
 
 export default SearchScreen;
