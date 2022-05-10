f16110a1e69050681042a669bba576be98147db9
inbum <1234!@>
2022-05-10 17:07:51 +0900

    7.4.2 완료

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, useWindowDimensions, View} from 'react-native';
 
 function SearchHeader() {
+    const {width} = useWindowDimensions();
+
     return (
-        <Text style={styles.block}> Hello </Text>
+        <View style={[styles.block, {width: width - -32, height: 24}]} />
     );
 }
 const styles = StyleSheet.create({
     block: {
-        color: 'white',
         backgroundColor: 'blue',
     },
 });
